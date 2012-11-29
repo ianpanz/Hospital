5d70173bfd5c80584556ecbce633d844281f4a8d
Ian S Panzica <paintballmastr56@Ian-S-Panzicas-MacBook-Pro.local>
2012-11-29 10:19:51 -0500

    Initial Commit

---
+++ Hospital/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  Hospital
+//
+//  Created by Ian Panzica on 11/29/12.
+//  Copyright (c) 2012 Cornell University . All rights reserved.
+//
+
+#include <iostream>
+
+int main (int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
